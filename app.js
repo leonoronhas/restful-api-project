5562d9b276288c9d25f9e50f946aac9dba130f51
Leonardo <leosud099@gmail.com>
2020-07-20 22:06:09 -0600

    test connection to db working

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-//Routes '/' = default route
-app.get('/', (request, response) => {
-    response.send('Hello from home!');
+// Routes '/' = default route
+app.get("/", (request, response) => {
+  response.send("Hello from home!");
 });
+
+// Connect to DB
+mongoose.connect(
+  "mongodb+srv://dbUser:Gentegrande88@cluster0.dtwta.mongodb.net/restfulDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, () => {
+      console.log('Connected to DB finally!')
+  }
+);
 
 app.listen(3000);
